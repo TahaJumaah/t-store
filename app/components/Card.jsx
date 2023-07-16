e6ea1783a942f6e82ba351bae4a696b08855fbb8
Taha Jumaah <taharasheed95@gmail.com>
2023-07-16 17:26:49 +0200

    working on the card layout, general layout is done.

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,8 +1,19 @@
 import styles from "./Card.module.css";
+import Image from "next/image";
+import shoppingCart from "../../public/shoppingcart.png";
 export default function Card() {
   return (
     <div className={styles.cardcontainer}>
-      <h1>Card</h1>
+      <div className={styles.cardimage}></div>
+      <div className={styles.cardtitle}>
+        <p>Title Here</p>
+      </div>
+      <div className={styles.cardtools}>
+        <Image
+          className={styles.cardtools_shoppingcart}
+          src={shoppingCart}
+        ></Image>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+import Card from "./components/Card";
 export default function Home() {
-  return <h1>Home is here</h1>;
+  return (
+    <>
+      <h1>This is still home no matter what</h1>
+      <Card></Card>
+    </>
+  );
 }
