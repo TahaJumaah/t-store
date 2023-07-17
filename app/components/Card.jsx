619acd5f479f394a89f8299a7a3b9090b4af6ae3
Taha Jumaah <taharasheed95@gmail.com>
2023-07-17 16:59:25 +0200

    Now the card renders a list from an object.

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,19 +1,27 @@
 import styles from "./Card.module.css";
 import Image from "next/image";
 import shoppingCart from "../../public/shoppingcart.png";
+import things from "@/public/database";
+
 export default function Card() {
-  return (
-    <div className={styles.cardcontainer}>
-      <div className={styles.cardimage}></div>
-      <div className={styles.cardtitle}>
-        <p>Title Here</p>
-      </div>
-      <div className={styles.cardtools}>
-        <Image
-          className={styles.cardtools_shoppingcart}
-          src={shoppingCart}
-        ></Image>
-      </div>
-    </div>
-  );
+  const myThings = things.map((thing) => {
+    return (
+      <>
+        <div className={styles.cardcontainer}>
+          <div className={styles.cardimage}></div>
+          <div className={styles.cardtitle}>
+            <p>{thing.title}</p>
+          </div>
+          <div className={styles.cardtools}>
+            <Image
+              className={styles.cardtools_shoppingcart}
+              src={shoppingCart}
+            ></Image>
+          </div>
+        </div>
+      </>
+    );
+  });
+
+  return <div className={styles.maincontainer}>{myThings}</div>;
 }
---
+++ public/database.jsx
@@ -0,0 +1,16 @@
+let things = [
+  {
+    id: 1,
+    title: "nVidia",
+    price: 200,
+    imagesrc: "",
+  },
+  {
+    id: 2,
+    title: "AMD",
+    price: 195,
+    imagesrc: "",
+  },
+];
+
+export default things;
